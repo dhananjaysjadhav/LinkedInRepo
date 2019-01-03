904285e3c60cf3d655d25507596edd8c33c21f7e
Dhananjay <dhananjayj84@gmail.com>
2019-01-03 17:49:38 +0530

    Selenium Basic code

---
+++ Java Basics/src/Basics1.java
@@ -0,0 +1,21 @@
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Navigated to header");
+		
+		//calling a method
+		
+		Methods m = new Methods();
+		m.ValidateHeader();
+		
+		System.out.println(m.ValidateFooter());
+		
+		
+			
+	}
+
+}
---
+++ Java Basics/src/Methods.java
@@ -0,0 +1,22 @@
+
+//sample method to be called from basics1
+
+public class Methods {
+	
+	public void ValidateHeader() 
+	
+	{
+	
+		System.out.println("Validate Header");
+		
+	}
+
+	public String ValidateFooter()	{
+		System.out.println("Validate footer");
+		return "Pass"; // Instead of Void it used String since we are returning something
+		
+	}
+	
+
+}
+
